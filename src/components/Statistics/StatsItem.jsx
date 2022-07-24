f26efbd870ade6c6c84f2df56ce0205086134fa8
ZABILA PETRO <petrozabila@gmail.com>
2022-07-24 17:02:16 +0300

    HW fixed

--- src/components/Statistics/StatsItem.jsx
+++ src/components/Statistics/StatsItem.jsx
@@ -4,7 +4,8 @@ import css from './Statistics.module.css';
 export default function StatsItem({title, stats}) {
     return (
         <>
-            <span className={css.title}>{title}</span>
+            {title && (<span className={css.title}>{title}</span>)}
+            
             <span className={css.percentage}>{stats}%</span>
         </>
     )
